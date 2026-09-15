e7d847149534db028a7b12a8ca16ef28dfd98a4c
29-29 <ackx29sodo@gmail.com>
2026-09-15 19:38:17 +0800

    some party colors

--- src/components/contestPieChart.vue
+++ src/components/contestPieChart.vue
@@ -12,9 +12,44 @@ const props = defineProps<{
   options?: ApexOptions
 }>()
 
+const partyColors = {
+  'UBJP': '#2a7115',
+  'BFP': '#2a4383',
+  'BGC': '#fac82a',
+  'MAHARDIKA': '#9ca3af',
+  'RAAYAT DEMOCRATIC PARTY': '#43de79',
+  'BAPA': '#ff0000',
+  'PRO BANGSAMORO PARTY': '#3a9f5b',
+  'MORO AKO': '#705507',
+  'BEST PARTY': '#a87731'
+}
+
+const getPartyColor = (key: string | undefined): string => {
+  if (key && key in partyColors) {
+    return partyColors[key as keyof typeof partyColors];
+  }
+  return '#9ca3af'; // Fallback gray
+};
+
+const computedColors = computed(() => {
+  return props.contest?.candidates.map(c => {
+    if (props.contest?.category == 'BARMM REGIONAL PARLIAMENT') {
+      if (c.name in partyColors) {return getPartyColor(c.name)}
+      else return '#9ca3af'
+    } else {
+      const match = c.name.match(/\(([^)]+)\)/)
+      if (match) {
+        const extracted = match[1];
+        return getPartyColor(extracted)
+      } else return '#9ca3af'
+    }
+  })
+})
+
 const options = computed<ApexOptions>(() => ({
   ...props.options,
   labels: props.contest?.candidates.map(x => x.name) ?? [],
+  colors: computedColors.value?.filter(((c): c is string => Boolean(c))) ?? [],
   tooltip: {
     y: {
       formatter: x => Number(x).toLocaleString()
@@ -29,6 +64,7 @@ const options = computed<ApexOptions>(() => ({
 
 const series = computed(() => props.contest?.candidates.map(x => x.votes))
 </script>
+
 <template>
   <q-card flat bordered>
     <q-card-section>
